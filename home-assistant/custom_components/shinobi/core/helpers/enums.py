67746c31f8e8282007bf0c9f80f9360f396d4052
Varun Sridharan <varunsridharan23@gmail.com>
2022-06-11 10:43:56 +0530

    💾  Config Updated

--- home-assistant/custom_components/shinobi/core/helpers/enums.py
+++
@@ -1,9 +0,0 @@
-from homeassistant.backports.enum import StrEnum
-
-
-class EntityStatus(StrEnum):
-    EMPTY = "empty"
-    READY = "ready"
-    CREATED = "created"
-    DELETED = "deleted"
-    UPDATED = "updated"
