33f99c8f1f086f4d67f5f6a55be5fd6ebc13c4cd
Varun Sridharan <varunsridharan23@gmail.com>
2022-06-13 06:00:03 +0530

    💾  Config Updated

--- home-assistant/integrations/shell_scripts/pfsense-backup.sh
+++ home-assistant/integrations/shell_scripts/pfsense-backup.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-source /root/scripts/functions.sh
+source /config/integrations/shell_scripts/inc/functions.sh
 
-REPOSITORY="vs-backups/pfsense"
+REPOSITORY="$(ha_secrets "pfsense_backup_repo")"
+ROUTER_IP="$(ha_secrets "pfsense_router_ip")"
+ROUTER_USERNAME="$(ha_secrets "pfsense_username")"
 REPOSITORY_FOLDER="$(locate_repo "$REPOSITORY")"
 
 setup_github_repo "$REPOSITORY" "PFSense Auto Backup" "githubactionbot+pfsense@gmail.com"
-run_ssh_cmd "$REPOSITORY" "scp pfsensebackup@10.0.100.1:/cf/conf/config.xml /tmp/pfsensetmp.xml"
+
+run_ssh_cmd "$REPOSITORY" "scp ${ROUTER_USERNAME}@${ROUTER_IP}:/cf/conf/config.xml /tmp/pfsensetmp.xml"
 
 mv /tmp/pfsensetmp.xml "$REPOSITORY_FOLDER/config.xml"
+
 cd "$REPOSITORY_FOLDER"
 
 if [ "$(git status --porcelain)" != "" ]; then
-    git add "$REPOSITORY_FOLDER/config.xml"
-    git commit -m "💾 Configuration Backup"
-   push_repo "$REPOSITORY"
-fi
+  git add "$REPOSITORY_FOLDER/config.xml"
+  git commit -m "💾 Configuration Backup"
 
-git tag -a "$(date +"%Y.%m.%d.%H.%M.%p")" -m "Auto Backup On $(date +"%B - %d/%m/%Y") @  $(date +%r)"
-push_repo "$REPOSITORY" " --tags"
+  push_repo "$REPOSITORY"
+
+  git tag -a "$(date +"%Y.%m.%d.%H.%M.%p")" -m "Auto Backup On $(date +"%B - %d/%m/%Y") @  $(date +%r)"
+  push_repo "$REPOSITORY" " --tags"
+fi
