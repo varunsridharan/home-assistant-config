d9f8a6e7a7c00f914b97f5fef316ffa64b56693a
Varun Sridharan <varunsridharan23@gmail.com>
2022-07-21 06:00:06 +0530

    💾  Config Updated

--- home-assistant/integrations/shell_scripts/vault1-status.sh
+++ home-assistant/integrations/shell_scripts/vault1-status.sh
@@ -7,4 +7,5 @@ VAULT1_USER="$(ha_secrets "vault1_user")"
 VAULT1_PASS="$(ha_secrets "vault1_pass")"
 
 # Power Down Vault 1
-run_ssh_cmd_pass "${VAULT1_IP}" "${VAULT1_USER}" "${VAULT1_PASS}" "shutdown -h now"
+#run_ssh_cmd_pass "${VAULT1_IP}" "${VAULT1_USER}" "${VAULT1_PASS}" "shutdown -h now"
+run_ssh_cmd_pass "${VAULT1_IP}" "${VAULT1_USER}" "${VAULT1_PASS}" "shutdown -h"
